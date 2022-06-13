b11bcf76d15b05ebb00cdf0f773c063c4a59a788
Yoris95 <98592375+Yoris95@users.noreply.github.com>
2022-06-13 22:24:00 +0800

    Delete day_5 directory

--- day_5/list.py
+++
@@ -1,8 +0,0 @@
-countries = ['Germany', 'France','Belgium','Sweden','Denmark','Finland','Norway','Iceland','Estonia']
-gr, fr, bg, sw, *scandic, es = countries
-print(gr)
-print(fr)
-print(bg)
-print(sw)
-print(scandic)
-print(es)
