a70bb520aba8cd90741afcd018ce45fccadd4ed6
Yoris95 <98592375+Yoris95@users.noreply.github.com>
2022-06-13 22:23:52 +0800

    Delete day_4 directory

--- day_4/strings.py
+++
@@ -1,9 +0,0 @@
-a = 4
-b = 3
-print(f'{a} + {b} = {a +b}')
-print(f'{a} - {b} = {a - b}')
-print(f'{a} * {b} = {a * b}')
-print(f'{a} / {b} = {a / b:.2f}')
-print(f'{a} % {b} = {a % b}')
-print(f'{a} // {b} = {a // b}')
-print(f'{a} ** {b} = {a ** b}')
