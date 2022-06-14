300e8bfd34a6b023c1ee663219c4ad787d1f02fc
Yoris95 <98592375+Yoris95@users.noreply.github.com>
2022-06-14 12:05:54 +0800

    Update Loops.py

--- 10_Day/Loops.py
+++ 10_Day/Loops.py
@@ -1,19 +1,19 @@
 # Exercise Level 1.
 # 1. Iterate 0 to 10 using for loop, do the same using while loop.
-# First solution of "For" loop.
+# First solution of "For" loop:
 k = [0,1,2,3,4,5,6,7,8,9,10]
 for i in k:
     print(i)                    
                                 # End.
 
 
-# Second solution of "For" loop.
+# Second solution of "For" loop:
 for i in range(11):
     print(i)
                                 # End.
 
 
-# First solution of "While" loop.
+# First solution of "While" loop:
 number = 0
 while number < 11:
     print(number)
@@ -21,26 +21,26 @@ while number < 11:
                                 # End.
 
 # 2. Iterate 10 to 0 using for loop, do the same using while loop.
-# First solution of "For" loop.
+# First solution of "For" loop:
 k = [10,9,8,7,6,5,4,3,2,1,0]
 for i in k:
     print(i)
                                 # End.
 
-# Second solution of "For" loop
+# Second solution of "For" loop:
 n = 0
 for n in reversed(range(11)):
     print(n) 
                                 # End.
 
-# First solution of "While" loop.
+# First solution of "While" loop:
 n = 11
 while n >= 0:
     print(n)
     n = n - 1 
                                 # End.
 
-# Second solution of "While" loop.
+# Second solution of "While" loop:
 n = 0
 while n <= 10:
     print(10 - n)
@@ -48,3 +48,27 @@ while n <= 10:
                                 # End.
 
 # 3. Write a loop that makes seven calls to print(), so we get on the output the following triangle:
+# First solution:
+a = "#"
+b = "##"
+c = "###"
+d = "####"
+e = "#####"
+f = "######"
+g = "#######"
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+                                # End.
+
+# Second solution: 
+n = 0
+while n < 7:
+    print("#" * (n + 1))
+    n = n + 1
+                                # End.
