819e72606b25aab099178f8ba46e5c16111aac63
Yoris95 <98592375+Yoris95@users.noreply.github.com>
2022-06-23 22:44:46 +0800

    Update conditionals.py

--- 09_Day/conditionals.py
+++ 09_Day/conditionals.py
@@ -61,3 +61,54 @@ elif a < b:
 else:
     print(a, "is equal to", b)
                                         # End.
+ 
+# Level 2
+# 1. Write a code which gives grade to students according to theirs scores:
+'''
+80-100, A
+70-89, B
+60-69, C
+50-59, D
+0-49, F
+'''
+# Solution:
+name = str(input("What's your name: "))
+score = int(input("What's your score: "))
+
+if 80 <= score <= 100:
+    print("You get A, congratulations {}!" .format(name))
+elif 70 <= score <= 79:
+    print("You get B, well done {}!" .format(name))
+elif 60 <= score <= 69:
+    print("You get C, not bad {}!" .format(name))
+elif 50 <= score <= 59:
+    print("You get D, Try harder {}!" .format(name))
+elif 0 <= score <= 49:
+    print("You get F, Unlucky {}!" .format(name))
+else:
+    print("Error, try again!")
+                                                            # End.
+
+# 2. Check if the season is Autumn, Winter, Spring or Summer. If the user input is: September, October or November, the season is Autumn. December, January or February, the season is Winter. March, April or May, the season is Spring June, July or August, the season is Summer
+# Solution:
+print("(Months format: January, February, March, April, May, June, July, August, September, October, November, December)")
+month = str(input("What month is it now:"))
+
+if month == ("September" and "October" and "November"):
+    print("The season is Autumn")
+elif month == ("December" and "January" and "February"):
+    print("This season is Winter")
+elif month == ("March" and "April" and "May"):
+    print("This season is Spring")
+elif month == ("June" and "July" and "August"):
+    print("This season is Summer")
+else:
+    print("Wrong input!")
+                                                            # End.
+
+# 3. The following list contains some fruits:
+'''
+fruits = ['banana', 'orange', 'mango', 'lemon']
+If a fruit doesn't exist in the list add the fruit to the list and print the modified list. If the fruit exists print('That fruit already exist in the list')
+'''
+
