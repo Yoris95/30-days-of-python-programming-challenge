0a33175a92b128906cbd0199a6e2ea72a16c6db1
Yoris95 <98592375+Yoris95@users.noreply.github.com>
2022-07-15 00:37:14 +0800

    Update list.py

--- 05_Day/list.py
+++ 05_Day/list.py
@@ -1,8 +1,35 @@
-countries = ['Germany', 'France','Belgium','Sweden','Denmark','Finland','Norway','Iceland','Estonia']
-gr, fr, bg, sw, *scandic, es = countries
-print(gr)
-print(fr)
-print(bg)
-print(sw)
-print(scandic)
-print(es)
+# Exercises: List
+# Level 1
+
+# 1. Declare an empty list
+# Solution 1
+empty_list = []
+print(empty_list)
+                        # End.
+# Solution 2
+empty_list = list()
+print(empty_list)
+                        # End.
+
+# 2. Declare a list with more than 5 items
+items = ["TV", "Radio", "Monitor", "Mouse", "Charger"]
+print(items)
+                        # End.
+
+# 3. Find the length of your list 
+items = ["TV", "Radio", "Monitor", "Mouse", "Charger"]
+print(len(items))
+                        # End.
+
+# 4. Get the first item, the middle item and the last item of the list
+items = ["TV", "Radio", "Monitor", "Mouse", "Charger"]
+first_item, second_item, third_item, fourth_item, fifth_item = items
+print(items[0])
+print(items[2])
+print(items[4])
+print("Or like this:")
+print("=============")
+print(first_item)
+print(third_item)
+print(fifth_item)
+                        # End.
